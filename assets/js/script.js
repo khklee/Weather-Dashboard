6a14ab48c75a281c09165fb3d42d1b1f772ee7f4
Kelly Lee <amorfati38@gmail.com>
2022-01-20 21:03:51 -0700

    fix finding city in weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city")
 
+// request geographical coordinates (lat, lon)
+var getLatLon = function(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=1&appid=4d7dc3e3b9de372c87b2cd21104c5026";
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data)
+
+                    // get lat & lon from a city name
+                    var cityLat = data[0].lat;
+                    var cityLon = data[0].lon;
+                    // get city's weather
+                    getCity(cityLat, cityLon);
+                })
+            }
+        })
+};
+
+
 // request a weather from the URL
-var getCity = function(city) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4d7dc3e3b9de372c87b2cd21104c5026";
+var getCity = function(lat, lon) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=4d7dc3e3b9de372c87b2cd21104c5026";
     fetch(apiURL)
         .then(function(response) {
             if (response.ok) {
@@ -16,13 +36,14 @@ var getCity = function(city) {
         })
 }
 
-// get value from input 
+// get values from city input 
 var citySubmitHandler = function(event) {
     event.preventDefault();
 
     var cityName = cityInputEl.value.trim();
+
     if (cityName) {
-        getCity(cityName);
+        getLatLon(cityName);
         cityInputEl.value = "";
     } else {
         alert("Please enter a city name.")
@@ -30,12 +51,12 @@ var citySubmitHandler = function(event) {
 }
 
 
+
 // var displayWeather = function() {
 // }
 
 
 searchFormEl.addEventListener("submit", citySubmitHandler);
-
 
 
 
