07340c61ce7b8351121f61c3aa1b5c347ecc1bac
Kelly Lee <amorfati38@gmail.com>
2022-01-21 00:36:35 -0700

    display current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city")
+var weatherEl = document.querySelector("#weather-container")
+var today = new Date();
 
 // request geographical coordinates (lat, lon)
 var getLatLon = function(city) {
@@ -23,12 +25,19 @@ var getLatLon = function(city) {
 
 // request a weather from the URL
 var getCity = function(lat, lon) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=4d7dc3e3b9de372c87b2cd21104c5026";
+    var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=4d7dc3e3b9de372c87b2cd21104c5026";
     fetch(apiURL)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data)
+
+                    // display current weather
+                    displayWeather(data);
+                    
+                    // clear the search field
+                    cityInputEl.value = "";
+
                 })
             } else {
                 alert("City Name Not Found!");
@@ -44,17 +53,54 @@ var citySubmitHandler = function(event) {
 
     if (cityName) {
         getLatLon(cityName);
-        cityInputEl.value = "";
     } else {
         alert("Please enter a city name.")
     }
 }
 
+// display current weather of a city
+var displayWeather = function(data) {
 
+    // clear old data
+    weatherEl.textContent = "";
 
-// var displayWeather = function() {
-// }
+    // creat current weather container
+    var curWeather = document.createElement("div");
+    var cityDate = document.createElement("h3");
+    var weatherImg = document.createElement("img");
+    var temp = document.createElement("p");
+    var wind = document.createElement("p");
+    var humidity = document.createElement("p");
+    var uv = document.createElement("p");
+    var uvIndex = document.createElement("span");
 
+    curWeather.classList = "current-weather";
+    cityDate.classList = "city";
+    
+    // UV conditions indicator
+    if (0 <= uvIndex < 3) {
+        uvIndex.classList = "favorable";
+    } else if (3 < uvIndex < 8) {
+        uvIndex.classList = "moderate";
+    } else {
+        uvIndex.classList = "severe";
+    };
+    
+    weatherImg.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png")
+
+    cityDate.textContent = city.value + " (" + (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear() + ")";
+    temp.textContent = "Temp: " + data.current.temp + "°F";
+    wind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    humidity.textContent = "Humidity: " + data.current.humidity + "%";
+    uv.textContent = "UV Index: "
+    uvIndex.textContent = data.current.uvi
+
+    weatherEl.appendChild(curWeather);
+    curWeather.appendChild(cityDate);
+    cityDate.appendChild(weatherImg);
+    curWeather.append(temp, wind, humidity, uv);
+    uv.appendChild(uvIndex);
+}
 
 searchFormEl.addEventListener("submit", citySubmitHandler);
 
